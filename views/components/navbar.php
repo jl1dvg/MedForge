6359fd67aa10778b1e3e78c3f0f894114a7ca0f3
jorgeluisdevera <jl1dvg@gmail.com>
2025-06-26 14:12:41 -0500

    Actualizacion Informe scraping IESS

--- views/components/navbar.php
+++ views/components/navbar.php
@@ -17,67 +17,66 @@ require_once __DIR__ . '/../../bootstrap.php';
         <li><a href="#"><i
                         class="mdi mdi-account-multiple"><span
                             class="path1"></span><span class="path2"></span></i>Pacientes</a>
-            <uL>
-                <li><a href="<?php echo BASE_URL . 'views/pacientes/lista.php'; ?>"><i class="mdi mdi-account-multiple"><span
+            <ul>
+                <li><a href="<?php echo BASE_URL . 'views/pacientes/lista.php'; ?>"><i
+                                class="mdi mdi-account-circle"><span
                                     class="path1"></span><span class="path2"></span></i>Lista de Pacientes</a>
                 </li>
                 <li><a href="<?php echo BASE_URL . 'views/pacientes/flujo/flujo.php'; ?>"><i
-                                class="mdi mdi-account-multiple"><span
+                                class="mdi mdi-account-convert"><span
                                     class="path1"></span><span class="path2"></span></i>Flujo de Pacientes</a>
                 </li>
-            </uL>
+            </ul>
         </li>
-        <li><a href="<?php echo BASE_URL . 'views/main/repots/solicitudes.php'; ?>"><i
-                        class="mdi mdi-file-chart"><span
-                            class="path1"></span><span class="path2"></span></i>Reportes y Solicitudes</a>
-            <uL>
+        <li><a href="#"><i class="mdi mdi-hospital-building"><span
+                            class="path1"></span><span class="path2"></span></i>Cirugías</a>
+            <ul>
+                <li><a href="<?php echo BASE_URL . 'views/editor/lista_protocolos.php'; ?>"><i
+                                class="mdi mdi-timetable"><span
+                                    class="path1"></span><span class="path2"></span></i>Solicitudes de Cirugía</a></li>
                 <li><a href="<?php echo BASE_URL . 'views/reportes/cirugias.php'; ?>"><i
                                 class="mdi mdi-table-large"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Reporte de Protocolos</a></li>
+                                    class="path2"></span></i>Protocolos Realizados</a></li>
                 <li><a href="<?php echo BASE_URL . 'views/editor/lista_protocolos.php'; ?>"><i
-                                class="mdi mdi-timetable"><span
+                                class="mdi mdi-tooltip-edit"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Solicitudes de Cirugía</a></li>
-            </uL>
-        </li>
-        <li><a href="<?php echo BASE_URL . 'views/editor/lista_protocolos.php'; ?>"><i
-                        class="mdi mdi-tooltip-edit"><span
-                            class="path1"></span><span class="path2"></span></i>Protocolos Quirúrgicos</a>
+                                    class="path2"></span></i>Plantillas de Protocolos</a></li>
+            </ul>
         </li>
         <li><a href="<?php echo BASE_URL . 'views/insumos/insumos.php'; ?>"><i class="mdi mdi-medical-bag"><span
                             class="path1"></span><span class="path2"></span></i>Gestión de Insumos</a>
-            <uL>
+            <ul>
                 <li><a href="<?php echo BASE_URL . 'views/insumos/insumos.php'; ?>"><i
-                                class="mdi mdi-clipboard-list-outline"><span
+                                class="mdi mdi-pharmacy"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Insumos</a></li>
-            </uL>
+                                    class="path2"></span></i>Lista de Insumos</a></li>
+            </ul>
         </li>
         <li><a href="#"><i
-                        class="mdi mdi-file-document"><span
-                            class="path1"></span><span class="path2"></span></i>Informes</a>
-            <uL>
+                        class="mdi mdi-file-chart"><span
+                            class="path1"></span><span class="path2"></span></i>Facturación por Afiliación</a>
+            <ul>
                 <li><a href="<?php echo BASE_URL . 'views/informes/informe_isspol.php'; ?>"><i
                                 class="mdi mdi-file-chart"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Consolidado ISSPOL</a></li>
+                                    class="path2"></span></i>ISSPOL</a></li>
                 <li><a href="<?php echo BASE_URL . 'views/informes/informe_issfa.php'; ?>"><i
                                 class="mdi mdi-file-chart"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Consolidado ISSFA</a></li>
+                                    class="path2"></span></i>ISSFA</a></li>
                 <li><a href="<?php echo BASE_URL . 'views/informes/informe_iess.php'; ?>"><i
                                 class="mdi mdi-file-chart"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Consolidado IESS</a></li>
+                                    class="path2"></span></i>IESS</a></li>
                 <li><a href="<?php echo BASE_URL . 'views/informes/informe_particulares.php'; ?>"><i
                                 class="mdi mdi-file-chart"><span
                                     class="path1"></span><span
-                                    class="path2"></span></i>Antenciones Particulares</a></li>
-            </uL>
+                                    class="path2"></span></i>Particulares</a></li>
+            </ul>
         </li>
         <?php if (in_array($_SESSION['permisos'] ?? '', ['administrativo', 'superuser'])): ?>
-            <li><a href="#"><i class="mdi mdi-settings"></i>Configuración</a>
+            <li><a href="#"><i class="mdi mdi-settings"></i>Administración</a>
                 <ul>
                     <li><a href="<?php echo BASE_URL . 'views/users/index.php'; ?>"><i class="mdi mdi-account-key"></i>Usuarios</a>
                     </li>
