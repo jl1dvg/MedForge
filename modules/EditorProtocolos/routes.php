edc41ee07251c36792e8ce82a99d041ba385a686
jorgeluisdevera <jl1dvg@gmail.com>
2025-11-02 17:58:27 -0500

    Creacion modulo Editor de protocolos

--- modules/EditorProtocolos/routes.php
+++ modules/EditorProtocolos/routes.php
@@ -2,26 +2,25 @@
 
 use Core\Router;
 use Modules\EditorProtocolos\Controllers\EditorController;
-use PDO;
 
 return static function (Router $router) {
-    $router->get('/protocolos', function (PDO $pdo) {
+    $router->get('/protocolos', function (\PDO $pdo) {
         (new EditorController($pdo))->index();
     });
 
-    $router->get('/protocolos/crear', function (PDO $pdo) {
+    $router->get('/protocolos/crear', function (\PDO $pdo) {
         (new EditorController($pdo))->create();
     });
 
-    $router->get('/protocolos/editar', function (PDO $pdo) {
+    $router->get('/protocolos/editar', function (\PDO $pdo) {
         (new EditorController($pdo))->edit();
     });
 
-    $router->post('/protocolos/guardar', function (PDO $pdo) {
+    $router->post('/protocolos/guardar', function (\PDO $pdo) {
         (new EditorController($pdo))->store();
     });
 
-    $router->post('/protocolos/eliminar', function (PDO $pdo) {
+    $router->post('/protocolos/eliminar', function (\PDO $pdo) {
         (new EditorController($pdo))->delete();
     });
 };
