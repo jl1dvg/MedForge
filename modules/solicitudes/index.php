eb1c441642b394fb49859dd39da4ec066cf0e7d3
jorgeluisdevera <jl1dvg@gmail.com>
2025-11-11 18:51:42 -0500

    soluciones

--- modules/solicitudes/index.php
+++ modules/solicitudes/index.php
@@ -13,7 +13,54 @@ define('SOLICITUDES_MODELS', $modulePath . '/models');
 define('SOLICITUDES_HELPERS', $modulePath . '/helpers');
 
 // Cargar archivos base
-require_once SOLICITUDES_MODELS . '/ExamenesModel.php';
+$examenesModelCandidates = [
+    dirname(__DIR__) . '/examenes/models/ExamenesModel.php',
+    dirname(__DIR__) . '/examenes/Models/ExamenesModel.php',
+    SOLICITUDES_MODELS . '/ExamenesModel.php',
+];
+
+$examenesClassCandidates = [
+    'ExamenesModel',
+    'Models\\ExamenesModel',
+];
+
+$resolvedClass = null;
+
+foreach ($examenesClassCandidates as $className) {
+    if (class_exists($className, false)) {
+        $resolvedClass = $className;
+        break;
+    }
+}
+
+foreach ($examenesModelCandidates as $modelPath) {
+    if ($resolvedClass) {
+        break;
+    }
+
+    if (!is_file($modelPath)) {
+        continue;
+    }
+
+    require_once $modelPath;
+
+    foreach ($examenesClassCandidates as $className) {
+        if (class_exists($className, false)) {
+            $resolvedClass = $className;
+            break 2;
+        }
+    }
+}
+
+if (!$resolvedClass) {
+    $pathsList = implode(", ", $examenesModelCandidates);
+    throw new RuntimeException("No se pudo cargar ExamenesModel. Rutas probadas: {$pathsList}");
+}
+
+if ($resolvedClass === 'Models\\ExamenesModel' && !class_exists('ExamenesModel', false)) {
+    class_alias($resolvedClass, 'ExamenesModel');
+}
+
 require_once SOLICITUDES_CONTROLLERS . '/SolicitudController.php';
 require_once SOLICITUDES_HELPERS . '/SolicitudHelper.php';
 
