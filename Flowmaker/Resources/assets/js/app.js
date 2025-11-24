f43efeb52c584198ff1563c21f58e767a13b8e9d
jorgeluisdevera <jl1dvg@gmail.com>
2025-11-24 11:30:53 -0500

    Separacion modulo Whatsapp y Autorresponder

---
+++ Flowmaker/Resources/assets/js/app.js
@@ -0,0 +1,3 @@
+// JS principal del módulo Flowmaker
+// Aquí en el futuro puedes agregar tu lógica de frontend.
+console.log('Flowmaker app.js cargado');
---
+++ Flowmaker/public/build/assets/app-B1vxw0Ta.js
@@ -0,0 +1,1 @@
+console.log("Flowmaker app.js cargado");
