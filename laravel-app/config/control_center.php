d968189354ef7df22dcd78f004bc928f8b51d9d2
jorgeluisdevera <jl1dvg@gmail.com>
2026-07-01 01:48:27 -0500

    Use instance slug for control center state

--- laravel-app/config/control_center.php
+++ laravel-app/config/control_center.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'instance_slug' => env('CONTROL_CENTER_INSTANCE_SLUG', env('CONTROL_CENTER_CLIENT_SLUG')),
+    'instance_slug' => env('CONTROL_CENTER_INSTANCE_SLUG'),
     'state_cache_ttl' => (int) env('CONTROL_CENTER_STATE_CACHE_TTL', 60),
     'fallback_state' => env('CONTROL_CENTER_FALLBACK_STATE', 'production'),
     'allowlist' => [
